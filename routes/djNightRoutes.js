50cb52e494c7fa32a85781fc3da21ea3b5d109b3
Vipin Dagar <dagarv23@gmail.com>
2026-03-21 11:20:39 +0530

    Update djNightRoutes.js

--- routes/djNightRoutes.js
+++ routes/djNightRoutes.js
@@ -7,7 +7,7 @@ import { authorizeRoles } from "../middlewares/roleMiddleware.js";
 const router = express.Router();
 
 // Define your routes for DJ Night here
-router.post('/register', protect , authorizeRoles("admin","user"), createStarNightRegistration);
+router.post('/register', createStarNightRegistration);
 router.get('/getRegistrations', protect, authorizeRoles("admin","user") , getStarNightRegistrations);
 router.get('/checkIn/:id', protect, authorizeRoles("admin","user","scanner") , checkInStarNight);
 
